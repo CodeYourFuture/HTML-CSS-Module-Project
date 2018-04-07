fe8f53ba850094b5e4fb4265937e4e9b2971dcab
hasanein <hasaninqh@yahoo.com>
2018-04-08 00:16:53 +0100

    some unwanted files were deleted from img folder as requested

--- img/twit_files/ablk.js
+++
@@ -1,7 +0,0 @@
-(function resetAB() {
-    if(typeof __ppl !== "undefined" && typeof __ppl.Interface !== "undefined" && typeof __ppl.Interface.resetAB !== "undefined") {
-        __ppl.Interface.resetAB();
-    } else {
-        window.setTimeout(resetAB, 50);
-    }
-})();
