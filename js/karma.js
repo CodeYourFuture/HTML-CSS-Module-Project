396a48135a6a7c29d4920ed84a43d69bd9665bd9
Amanda Obiajulu Nwadukwe <xtremeli.basic@gmail.com>
2021-05-23 15:47:01 +0100

    finished html
    Amanda's has added finished with all the major parts of this website

---
+++ js/karma.js
@@ -0,0 +1,7 @@
+//Just an experiment that is not working yet
+const menu = document.getElementById("hamburger-menu");
+const nav = document.getElementById("nav");
+
+menu.addEventListener("click", () => {
+    nav.style.left.value = "0";
+})
