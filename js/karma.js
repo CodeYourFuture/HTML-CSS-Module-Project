c54891ac6f2413a74d43b4044e43dbc0b2e0f7a0
Amanda Obiajulu Nwadukwe <xtremeli.basic@gmail.com>
2021-05-27 22:31:31 +0100

    functional navigation
    Amanda added some function to the navigation bar

---
+++ js/karma.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var navigation = document.getElementById("nav");
+    var header = document.getElementById("header");
+    navigation.classList.toggle("navigation");
+    header.classList.toggle("fixed");
+ }
