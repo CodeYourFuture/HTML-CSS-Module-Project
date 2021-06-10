9918f22929eeff7246e5e39fdd3c509afc6634ea
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2021-06-10 22:56:49 +0100

    Mostly menu fixes
    Fixed poorly formatted html nav menu structure
    Added a (fun) if statement to fix a bug with the hamburger menu I discovered
    Tweaked some menu related media queries

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-// Declares hamburgerMenu and FullMenu variables then sets their value equal to the
-// Id's nav-hamburger-Menu and nav-full-menu.
 const hamburgerMenu = document.getElementById("nav-hamburger-Menu");
 const fullMenu = document.getElementById("nav-full-Menu");
 
-// Function to add click functionality to the hamburger menu. If the hamburger
-// menu is clicked, the class show is toggled.
+// Feels a bit hacky as I had to add this to ensure the menu loads closed and doesn't
+// break on first load if the screen size is greater than or equal to 769px.
+// Felt a little less hacky doing it like this (and fun!) although I realise I could have
+// done this in one line :-)
+let w = document.documentElement.clientWidth || window.innerWidth;
+
+if (w >= 769 || w < 769) {
+  fullMenu.classList.toggle("show");
+}
+
+// Add click functionality to the hamburger menu.
 hamburgerMenu.addEventListener("click", () => {
   fullMenu.classList.toggle("show");
 });
