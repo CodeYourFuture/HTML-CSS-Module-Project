32371b773f5a8a211e1dd7d6d24d66425f1d2074
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2021-06-11 08:52:12 +0100

    Transition improvements & hamburger Js tweaks

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const hamburgerMenu = document.getElementById("nav-hamburger-Menu");
 const fullMenu = document.getElementById("nav-full-Menu");
+const w = document.documentElement.clientWidth || window.innerWidth;
 
 // Feels a bit hacky as I had to add this to ensure the menu loads closed and doesn't
 // break on first load if the screen size is greater than or equal to 769px.
 // Felt a little less hacky doing it like this (and fun!) although I realise I could have
-// done this in one line :-)
-let w = document.documentElement.clientWidth || window.innerWidth;
-
-if (w >= 769 || w < 769) {
+// done this in one line with fullMenu.classList.toggle("show"); :-)
+if (w > 0) {
   fullMenu.classList.toggle("show");
 }
 
