31da1bdeffcb91b5a342c231ee01ed2bef8ab107
konikalily <konika.l.biswas@gmail.com>
2021-06-21 11:16:31 +0100

    changed the class name for get_karma section

--- hamburger_menu.js
+++ hamburger_menu.js
@@ -1,7 +1,6 @@
-
-var toggleButton = document.getElementsByClassName('toggle-button')[0];
-var navLink = document.getElementsByClassName('nav-link')[0];
-toggleButton.addEventListener('click',showMenu);
-function showMenu(){
-    navLink.classList.toggle("active");
+var toggleButton = document.getElementsByClassName("toggle-button")[0];
+var navLink = document.getElementsByClassName("nav-link")[0];
+toggleButton.addEventListener("click", showMenu);
+function showMenu() {
+  navLink.classList.toggle("active");
 }
