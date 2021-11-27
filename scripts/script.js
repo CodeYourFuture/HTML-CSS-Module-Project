63c94fceb3e40e03a8542a07b264c915c70d5037
Berkeli Halmyradov <berkelih@gmail.com>
2021-11-27 17:25:27 +0000

    Fixed issues raised from previous commit
    Hi @gixo,
    
    Thank you for the feedback, I have now gone through the comments and applied fixes.

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+
+window.onload=function(){
+    document.getElementById("menu-hamburger").addEventListener("click", toggleMenu);
+}
+
+const toggleMenu = () => {
+    var navBar=document.getElementById("navigation");
+    console.log("clicked")
+    if (navBar.classList.contains("nav-mobile")) {
+        navBar.classList.remove("nav-mobile");
+        navBar.classList.add("navigation");
+    } else {
+        navBar.classList.remove("navigation");
+        navBar.classList.add("nav-mobile");
+    }
+    window.addEventListener("resize", function(event) {
+    if (navBar.classList.contains("nav-mobile")) {
+        navBar.classList.remove("nav-mobile");
+        navBar.classList.add("navigation");
+    }
+})
+}
