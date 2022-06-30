99f95604433cfc4a4e2668aad0d4798bf89e471f
Kawa MANMI <39382602+kawaMANMI@users.noreply.github.com>
2022-06-30 04:25:09 +0100

    Cleaned the code

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ var btns = btnContainer.getElementsByClassName("navigation__item");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function () {
     var current = document.getElementsByClassName("active");
-    document.getElementById("demo").innerHTML = current.length;
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
   });
