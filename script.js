58457fbad436994d1b1e55a94d359c22dadecb08
Kawa MANMI <39382602+kawaMANMI@users.noreply.github.com>
2022-06-30 04:08:24 +0100

    almost done

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ var btnContainer = document.getElementById("navigation__list");
 
 // Get all buttons with class="btn" inside the container
 var btns = btnContainer.getElementsByClassName("navigation__item");
+
 // Loop through the buttons and add the active class to the current/clicked button
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function () {
     var current = document.getElementsByClassName("active");
+    document.getElementById("demo").innerHTML = current.length;
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
   });
